4cada985c526ddcb1d952a3e7fd9a4edf3528ee7
Phuong <phuongnhu0894@gmail.com>
2015-11-01 21:58:44 +0700

    First Commit

---
+++ IAmRich/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  IAmRich
+//
+//  Created by Phuong on 11/1/15.
+//  Copyright © 2015 Appcoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *pictureImageview;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topConstraint;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [UIView animateWithDuration:5 animations:^{
+        self.pictureImageview.alpha = 1;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:2 animations:^{
+            self.textLabel.alpha = 1;
+            self.topConstraint.constant = 20;
+            [self.view layoutIfNeeded];
+        }];
+    }];
+    
+    
+}
+
+
+
+@end
